a63fa746b34630fd458711f772387840c4fc53ca
Marcos Tzuc <marcos@virtualia.com>
2022-03-17 17:56:04 -0600

    Inicializando proyecto

---
+++ app/src/main/java/com/tcdev/mytrades/classes/Bitso.java
@@ -0,0 +1,16 @@
+package com.tcdev.mytrades.classes;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Bitso {
+
+    protected String ApiKey = "Marcos";
+    protected String Secret = "Tzuc";
+
+    public void message(Context context)
+    {
+        Toast.makeText(context, "Esta es una prueba de class", Toast.LENGTH_SHORT).show();
+    }
+
+}
