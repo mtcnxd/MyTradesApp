4c54b88d4bf84288914aa7163264a25d4edf9a29
Marcos Tzuc <marcos@virtualia.com>
2022-04-20 00:22:42 -0500

    Mejorando balances activity

--- app/src/main/java/com/tcdev/mytrades/HistoryActivity.java
+++ app/src/main/java/com/tcdev/mytrades/HistoryActivity.java
@@ -7,11 +7,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.tcdev.mytrades.TradesClass.Trades;
+import com.tcdev.mytrades.TradesClass.TradesAsyncTask;
+import com.tcdev.mytrades.TradesClass.TradesBalanceAdapter;
+import com.tcdev.mytrades.TradesClass.TradesBalanceClass;
 import com.tcdev.mytrades.databinding.ActivityHistoryBinding;
 
-public class HistoryActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
+public class HistoryActivity extends AppCompatActivity {
+    private ArrayList<TradesBalanceClass> arrayListBalance;
     private ActivityHistoryBinding binding;
 
     @Override
@@ -26,6 +35,8 @@ public class HistoryActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
 
+        loadListViewBalance();
+
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +45,31 @@ public class HistoryActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void loadListViewBalance(){
+        ListView listViewBalance = findViewById(R.id.listViewHistory);
+        listViewBalance.setDivider(null);
+
+        try {
+            TradesAsyncTask asyncTask = new TradesAsyncTask();
+            asyncTask.setPath("/api/wservice.php");
+            asyncTask.setData("balances");
+            String payload = asyncTask.execute().get();
+
+            Trades trades = new Trades();
+            arrayListBalance = trades.getBalanceArray(payload);
+
+            TradesBalanceAdapter adapter = new TradesBalanceAdapter (this, arrayListBalance);
+            listViewBalance.setAdapter(adapter);
+
+            int sizeOfPurchase = trades.getSizeOfPurchases();
+            Toast.makeText(this, sizeOfPurchase + " active purchases", Toast.LENGTH_SHORT).show();
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
